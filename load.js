7d0681fa85588b9597266c1908a21d32b705a66c
Michiel de Jong <michiel@ripple.com>
2017-02-16 16:49:45 +0100

    Prioritize Settlement methods

--- load.js
+++ load.js
@@ -5,12 +5,12 @@ xhr.onload = function() {
   var html = xhr.response.sort(function(a, b) {
     if ((a.settlements === 'HTTP <span style="color:red">404</span> response') && (b.settlements !== 'HTTP <span style="color:red">404</span> response')) { return 1; }
     if ((a.settlements !== 'HTTP <span style="color:red">404</span> response') && (b.settlements === 'HTTP <span style="color:red">404</span> response')) { return -1; }
-    // if ((a.settlements === 'None') && (b.settlements !== 'None')) { return 1; }
-    // if ((a.settlements !== 'None') && (b.settlements === 'None')) { return -1; }
     if ((a.health === 'OK') && (b.health !== 'OK')) { return -1; }
     if ((a.health !== 'OK') && (b.health === 'OK')) { return 1; }
     if ((a.ping) && (!b.ping)) { return -1; }
     if ((!a.ping) && (b.ping)) { return 1; }
+    if ((a.settlements === 'None') && (b.settlements !== 'None')) { return 1; }
+    if ((a.settlements !== 'None') && (b.settlements === 'None')) { return -1; }
     if (a.hostname < b.hostname) { return -1; }
     if (a.hostname > b.hostname) { return 1; }
     return 0;
@@ -18,8 +18,8 @@ xhr.onload = function() {
     `<tr><td><a href="https://${line.hostname}">${line.hostname}</a></td>` +
         `<td>${line.prefix}</td>` +
         `<td>${line.owner}</td>` +
-        `<td>${line.health.slice(0, 50)}</td>` +
         `<td>${line.settlements.slice(0, 50)}</td>` +
+        `<td>${line.health.slice(0, 50)}</td>` +
         (line.ping?`<td style="color:green">&#x2713;</td>` : `<td style="color:red">&#x2716;</td>`) +
         `</tr>`
   ).join('\n');
@@ -28,8 +28,8 @@ xhr.onload = function() {
     '<th>ILP Kit URL</th>',
     '<th>Ledger Prefix</th>',
     '<th>Owner\'s Connector Account</th>',
-    '<th>Health</th>',
     '<th>Settlement Methods</th>',
+    '<th>Health</th>',
     '<th>Ping</th>',
   ].join('\n');
 };
