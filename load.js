8cb383f892af88baca84af8d8c840f37e8aa10be
Michiel de Jong <michiel@ripple.com>
2017-02-16 15:50:04 +0100

    List settlement methods

--- load.js
+++ load.js
@@ -7,6 +7,7 @@ xhr.onload = function() {
         `<td>${line.prefix}</td>` +
         `<td>${line.owner}</td>` +
         (line.ping?`<td style="color:green">&#x2713;</td>` : `<td style="color:red">&#x2716;</td>`) +
+        `<td>${line.settlements.slice(0, 50)}</td>` +
         `</tr>`
   ).join('\n');
   document.getElementById("nodes-list").innerHTML = html;
@@ -15,6 +16,7 @@ xhr.onload = function() {
     '<th>Ledger Prefix</th>',
     '<th>Owner\'s Connector Account</th>',
     '<th>Ping</th>',
+    '<th>Settlement Methods</th>',
   ].join('\n');
 };
 xhr.send();
--- probe.js
+++ probe.js
@@ -1,7 +1,46 @@
 var fs = require('fs');
 var ping = require('ping');
+var https = require('https');
 
 const DATA_FILE = './data.json';
+
+function checkUrl(i, path, field) {
+  return new Promise((resolve) => {
+    var request = https.request({
+      hostname: hosts[i].hostname,
+      port:443,
+      path: path,
+      method: 'GET'
+    }, function(response) {
+      var str = '';
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+  
+      response.on('end', function () {
+        if (response.statusCode === 200) {
+          hosts[i][field] = str;
+        } else {
+          hosts[i][field] = `http ${response.statusCode} response`;
+        }
+        resolve();
+      });
+    });
+    request.setTimeout(5000, function(err) {
+      hosts[i][field] = 'Timed out';
+      resolve();
+    });
+    request.on('error', function(err) {
+      hosts[i][field] = 'Connection error';
+      resolve();
+    });
+    request.end();
+  });
+}
+
+function checkSettlements(i) {
+  return checkUrl(i, '/api/settlement_methods', 'settlements');
+}
 
 function pingHost(i) {
   return new Promise((resolve) => {
@@ -27,7 +66,9 @@ try {
 }
 var promises = [];
 for (var i=0; i<hosts.length; i++) {
+//for (var i=0; i<3; i++) {
   promises.push(pingHost(i));
+  promises.push(checkSettlements(i));
 }
 Promise.all(promises).then(() => {
   fs.writeFileSync(DATA_FILE, '[\n' + 
