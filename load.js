16cc76e128fad7ffb9a1cc6cfc13444db9ad5136
Michiel de Jong <michiel@ripple.com>
2017-02-16 19:07:31 +0100

    Display ilp-kit version and sort on its presence

--- load.js
+++ load.js
@@ -3,6 +3,8 @@ xhr.responseType = 'json';
 xhr.open('GET', './data.json', true);
 xhr.onload = function() {
   var html = xhr.response.sort(function(a, b) {
+    if ((('' + a.version).indexOf('<span style="color:green">') !== -1) && (('' + b.version).indexOf('<span style="color:green">') === -1)) { return -1; }
+    if ((('' + a.version).indexOf('<span style="color:green">') === -1) && (('' + b.version).indexOf('<span style="color:green">') !== -1)) { return 1; }
     if ((a.settlements.indexOf('<span style="color:red">') !== -1) && (b.settlements.indexOf('<span style="color:red">') === -1)) { return 1; }
     if ((a.settlements.indexOf('<span style="color:red">') === -1) && (b.settlements.indexOf('<span style="color:red">') !== -1)) { return -1; }
     if ((a.health === 'OK') && (b.health !== 'OK')) { return -1; }
@@ -16,6 +18,7 @@ xhr.onload = function() {
     return 0;
   }).map(line =>
     `<tr><td><a href="https://${line.hostname}">${line.hostname}</a></td>` +
+        `<td>${line.version}</td>` +
         `<td>${line.prefix}</td>` +
         `<td>${line.owner}</td>` +
         `<td>${line.settlements.slice(0, 50)}</td>` +
@@ -26,6 +29,7 @@ xhr.onload = function() {
   document.getElementById("nodes-list").innerHTML = html;
   document.getElementById("header-row").innerHTML = [
     '<th>ILP Kit URL</th>',
+    '<th>ILP Kit Version</th>',
     '<th>Ledger Prefix</th>',
     '<th>Owner\'s Connector Account</th>',
     '<th>Settlement Methods</th>',
--- probe.js
+++ probe.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var ping = require('ping');
 var https = require('https');
+var WebFinger = require('webfinger.js').WebFinger;
+var wf = new WebFinger();
 
 const DATA_FILE = './data.json';
 
@@ -16,7 +18,7 @@ function checkUrl(i, path) {
       response.on('data', function (chunk) {
         str += chunk;
       });
-  
+
       response.on('end', function () {
         resolve({ status: response.statusCode, body: str });
       });
@@ -51,10 +53,34 @@ function checkHealth(i) {
   });
 }
 
+function getApiVersion(i) {
+  return new Promise((resolve) => {
+    wf.lookup('https://'+hosts[i].hostname, function(err, result) {
+      if (err) {
+        resolve(`<span style="color:red">WebFinger error</span>`);
+        return;
+      }
+      var version
+      try {
+        version = result.object.properties['https://interledger.org/rel/protocolVersion'];
+      } catch(e) {
+        resolve(`<span style="color:red">WebFinger properties missing</span>`);
+        return;
+      }
+      if (typeof version === 'string') {
+        resolve(`<span style="color:green">${version}</span>`);
+      } else {
+        resolve(JSON.stringify(version));
+      }
+    });
+  }).then(text => {
+    hosts[i].version = text;
+  });
+}
+
 function checkSettlements(i) {
   return checkApiCall(i, 'settlements', '/api/settlement_methods', function(body) {
     var methods
-    console.log('parsing', hosts[i].hostname, body);
     try {
       methods = JSON.parse(body);
       if (methods.length === 0) {
@@ -64,7 +90,6 @@ function checkSettlements(i) {
         methods.map(obj => obj.name).join(', ') +
         '</span>';
     } catch(e) {
-      console.log(body, e);
       return '<span style="color:red">Unparseable JSON</span>';
     }
   });
@@ -91,15 +116,15 @@ try {
   process.exit(1);
 }
 var promises = [];
-//for (var i=6; i<9; i++) {
+//for (var i=16; i<17; i++) {
 for (var i=0; i<hosts.length; i++) {
-  console.log('checking', hosts[i].hostname);
+  promises.push(getApiVersion(i));
   promises.push(pingHost(i));
   promises.push(checkHealth(i));
   promises.push(checkSettlements(i));
 }
 Promise.all(promises).then(() => {
-  fs.writeFileSync(DATA_FILE, '[\n' + 
+  fs.writeFileSync(DATA_FILE, '[\n' +
     hosts.map(obj => `  ${JSON.stringify(obj)}`).join(',\n') +
     '\n]\n');
 });
